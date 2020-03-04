ebf137753d5ce74117e352436cae1351f48a7bf2
Josh busnelli <joshbusnelli@Joshs-MacBook-Pro.local>
2020-03-04 15:57:26 -0700

    fix

--- site/web/app/themes/ernemann/resources/assets/scripts/routes/ourApproach.js
+++ site/web/app/themes/ernemann/resources/assets/scripts/routes/ourApproach.js
@@ -3,46 +3,23 @@ export default {
   init() {
     // JavaScript to be fired on the about us page
 
-    function debounce(func, wait, immediate) {
-      var timeout;
-      return function() {
-      var context = this, args = arguments;
-      var later = function() {
-          timeout = null;
-          if (!immediate) func.apply(context, args);
-      };
-      var callNow = immediate && !timeout;
-      clearTimeout(timeout);
-      timeout = setTimeout(later, wait);
-      if (callNow) func.apply(context, args);
-      };
-  }
-         
-  var videoEl = document.getElementById('vid'),
-                videoContainer = $('video');
+    var _video = document.querySelector('video');
+      function isScrolledIntoView( element ) {
+          var elementTop    = element.getBoundingClientRect().top,
+              elementBottom = element.getBoundingClientRect().bottom;
+
+          return elementTop >= 0 && elementBottom <= window.innerHeight;
+      }
+      window.addEventListener('scroll', function(){
+        if (isScrolledIntoView(_video)) {
+          _video.play();
+        }
+        else {
+          _video.pause()
+        }
+      })
 
 
-  $.fn.isInViewport = function() {
-      var elementTop = $(this).offset().top;
-      var elementBottom = elementTop + $(this).outerHeight();
-
-      var viewportTop = $(window).scrollTop();
-      var viewportBottom = viewportTop + $(window).height();
-
-      return elementBottom > viewportTop && elementTop < viewportBottom;
-  };
-
-  $(window).on('resize scroll', debounce(function() {
-      if (videoContainer.isInViewport()) {
-          videoEl.play();
-          console.log('play');
-      } else {
-          videoEl.pause();
-          console.log('paused')
-      }   
-  }, 250))
-
-  
     const element = document.getElementById('tabs');
     const options = {
       buttonActiveClass: 'is-active',
